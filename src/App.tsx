4baf032eea0ffe53ea563b3bb41900856ddb7100
Mahdi Ghaharian <mahdighaharian@MBP-MGH.local>
2024-11-10 01:43:21 +0400

    added target fund

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { TonConnectButton } from '@tonconnect/ui-react';
 
 function App() {
   const projects = [
-    { id: 1, text: "Project 1", deadline: "2024-12-31" },
-    { id: 2, text: "Project 2", deadline: "2025-01-15" },
-    { id: 3, text: "Project 3", deadline: "2025-02-20" },
+    { id: 1, text: "Campaign 1", deadline: "2024-12-31", target: "122000 $" },
+    { id: 2, text: "Campaign 2", deadline: "2025-01-15", target: "25000 $" },
+    { id: 3, text: "Campaign 3", deadline: "2025-02-20", target: "53000 $" },
+    { id: 3, text: "Campaign 4", deadline: "2025-02-22", target: "70000 $" },
+    { id: 3, text: "Campaign 5", deadline: "2025-02-25", target: "6400 $" },
+    { id: 3, text: "Campaign 6", deadline: "2025-02-29", target: "120000 $" },
   ];
 
   return (
@@ -21,6 +24,7 @@ function App() {
           <div key={project.id} className="card">
             <h2>{project.text}</h2>
             <p>Deadline: {project.deadline}</p>
+            <p>Target Fund: {project.target}</p>
           </div>
         ))}
       </div>
