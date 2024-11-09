30a70d9d5c080e5e71e8e2e753c101b078205f31
Mahdi Ghaharian <mahdighaharian@MBP-MGH.local>
2024-11-10 01:25:43 +0400

    fixed the issues in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 // App.jsx
-import React, { useState } from "react";
 import "./App.css";
 import { TonConnectButton } from '@tonconnect/ui-react';
 
 function App() {
-  const [isWalletConnected, setWalletConnected] = useState(false);
-  const [projects, setProjects] = useState([
+  const projects = [
     { id: 1, text: "Project 1", deadline: "2024-12-31" },
     { id: 2, text: "Project 2", deadline: "2025-01-15" },
     { id: 3, text: "Project 3", deadline: "2025-02-20" },
-  ]);
-
-  const handleConnectWallet = () => {
-    setWalletConnected(true); // Simulate wallet connection
-  };
+  ];
 
   return (
     <div className="App">
